5547c7500648466e681bc00f1bbc466f9db02d9f
Alinal <alina542.1@gmail.com>
2012-01-12 19:23:52 +0200

    TwoCoins

---
+++ InferNet/InferNet/TwoCoins.cs
@@ -0,0 +1,28 @@
+﻿using MicrosoftResearch.Infer.Models;
+using MicrosoftResearch.Infer;
+
+
+namespace InferNetExamples
+{
+    public class TwoCoins
+    {
+        public object ForwardProbability(double firstDistribution, double secondDistribution)
+        {
+            var firstCoin = Variable.Bernoulli(firstDistribution);
+            var secondCoin = Variable.Bernoulli(secondDistribution);
+            var bothHeads = firstCoin & secondCoin;
+            var engine = new InferenceEngine();
+            return engine.Infer(bothHeads);
+        }
+
+        public object BackwardProbability(double firstDistribution, double secondDistribution, bool result)
+        {
+            var firstCoin = Variable.Bernoulli(firstDistribution);
+            var secondCoin = Variable.Bernoulli(secondDistribution);
+            var bothHeads = firstCoin & secondCoin;
+            bothHeads.ObservedValue = result;
+            var engine = new InferenceEngine();
+            return engine.Infer(firstCoin);
+        }
+    }
+}
---
+++ InferNet/InferNetTests/TwoCoinsTests.cs
@@ -0,0 +1,31 @@
+﻿using InferNetExamples;
+using Xunit;
+
+namespace InferNetExamplesTests
+{
+    public class TwoCoinsTests
+    {
+        private readonly TwoCoins twoCoins = new TwoCoins();
+        
+        [Fact]
+        public void ForwardProbability_CheckResult()
+        {
+            var result = twoCoins.ForwardProbability(0.5, 0.5);
+            Assert.Equal("Bernoulli(0,25)", result.ToString());
+        }
+
+        [Fact]
+        public void BackwardProbabilityObserveFalse_CheckResult()
+        {
+            var result = twoCoins.BackwardProbability(0.5, 0.5, false);
+            Assert.Equal("Bernoulli(0,3333)", result.ToString());
+        }
+
+        [Fact]
+        public void BackwardProbabilityObserveTrue_CheckResult()
+        {
+            var result = twoCoins.BackwardProbability(0.5, 0.5, true);
+            Assert.Equal("Bernoulli(1)", result.ToString());
+        }
+    }
+}

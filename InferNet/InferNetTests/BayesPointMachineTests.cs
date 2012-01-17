9541bc3c3569aaaec9236bb98c61d44b8abdd886
Alinal <alina542.1@gmail.com>
2012-01-17 21:50:26 +0200

    Accuracy of BayesPointMachineTests checked by test

--- InferNet/InferNetTests/BayesPointMachineTests.cs
+++ InferNet/InferNetTests/BayesPointMachineTests.cs
@@ -1,10 +1,6 @@
-using System.Collections.Generic;
 using InferNet;
-using MicrosoftResearch.Infer;
-using MicrosoftResearch.Infer.Distributions;
-using MicrosoftResearch.Infer.Maths;
-using MicrosoftResearch.Infer.Models;
 using Xunit;
+using System.Linq;
 
 namespace InferNetTests
 {
@@ -13,38 +9,29 @@ namespace InferNetTests
         [Fact]
         public void CheckTrain()
         {
+            //model data
             double[] incomes = { 63, 16, 28, 55, 22, 20 };
             double[] ages = { 38, 23, 40, 27, 18, 40 };
             bool[] willBuy = { true, false, true, true, false, false };
+
+            //test data
             double[] incomesTest = { 58, 18, 22 };
             double[] agesTest = { 36, 24, 37 };
+            bool[] yTest = { true, false, false };
+
+            //train and infer
             var machine = new BayesPointMachine();
             var model = machine.Train(incomes, ages, willBuy);
             var result= machine.Test(incomesTest, agesTest, model);
-            var r = result[0];
-            var d = r.GetProbTrue();
-        }
+            var resultInBool = result.Select(bernoulli => bernoulli.GetProbTrue() > 0.5 ? true : false).ToList();
 
-        [Fact]
-        public void Test()
-        {
-            bool[] ydata = { true, false, true, true, false, false };
-            var y = Variable.Observed(ydata);
-            var j = y.Range;
-            
-            double[] incomes = { 63, 16, 28, 55, 22, 20 };
-            double[] ages = { 38, 23, 40, 27, 18, 40 };
-            var xdata = new Vector[(incomes as IList<double>).Count];
-            for (var i = 0; i < xdata.Length; i++)
+            //check accuracy
+            var accuracy = 0;
+            for (var i = 0; i < resultInBool.Count(); i++)
             {
-                xdata[i] = Vector.FromList(new List<double> { incomes[i], ages[i], 1 });
+                accuracy += resultInBool[i] == yTest[i] ? 1:0;
             }
-            var res = Variable.Observed(xdata, j);
-
-            var w = Variable.Random(new VectorGaussian(Vector.Zero(3), PositiveDefiniteMatrix.Identity(3)));
-            var gaussianFromMeanAndVariance = Variable.GaussianFromMeanAndVariance(Variable.InnerProduct(w, res[j]), 0.5);
-            y[j] = gaussianFromMeanAndVariance > 0;
-            var inf = (VectorGaussian)(new InferenceEngine()).Infer(w);
+            Assert.Equal(100, accuracy*100/resultInBool.Count);
         }
     }
 }

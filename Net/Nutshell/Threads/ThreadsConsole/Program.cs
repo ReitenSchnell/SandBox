6e0e6c0ac8ae5a2070c4e0d54d0aa56c5b64ff05
alinal@cl <alina542.1@gmail.com>
2013-01-17 17:34:11 +0200

    Some simple threads code

---
+++ Net/Nutshell/Threads/ThreadsConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadsConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(DoY);
+            thread.Start();
+            WriteSymbol('x');
+            Console.Read();
+        }
+
+        private static void DoY()
+        {
+            WriteSymbol('y');
+        }
+
+        private static void WriteSymbol(char symbol)
+        {
+            Enumerable.Range(0, 1000).ToList().ForEach(i => Console.Write(symbol));
+        }
+    }
+}

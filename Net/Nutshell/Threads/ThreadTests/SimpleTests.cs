d1a4c988b6a37c91e5bfeaf5e44550354b07c94d
alinal@cl <alina542.1@gmail.com>
2013-01-24 12:27:38 +0200

    Threads with lock

---
+++ Net/Nutshell/Threads/ThreadTests/SimpleTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace ThreadTests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void test_shared_state()
+        {
+            var done = false;
+            ThreadStart action = () =>
+                                     {
+                                         if (!done)
+                                         {
+                                             done = true;
+                                             Console.Out.WriteLine("Done");
+                                         }
+                                     };
+            new Thread(action).Start();
+            action();
+        }
+
+        [Fact]
+        public void test_shared_state_with_lock()
+        {
+            var done = false;
+            var locker = new object();
+            ThreadStart action = () =>
+            {
+                lock (locker)
+                {
+                    if (done) return;
+                    done = true;
+                    Console.Out.WriteLine("Done");
+                }
+            };
+            new Thread(action).Start();
+            action();
+        }
+    }
+}
